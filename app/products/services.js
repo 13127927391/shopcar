bedf2faa7fa05eabb7f3e4a2c782645fe6602f16
shizhankui <474651027@qq.com>
2017-12-07 17:30:20 +0800

    Revert "shopcar"
    This reverts commit 3f9c12b593863d8269890b58c2b09de7f3921532.

--- app/products/services.js
+++
@@ -1,9 +0,0 @@
-angular.module("productServiceModule", [])
-       .constant("queryProductApi", "http://localhost/H510/api/product.php?a=query")
-       .service("productService", function($http, queryProductApi) {
-       		this.requestData = function(success, error){
-       			$http({
-       				url:queryProductApi,
-       			}).then(success, error)
-       		}
-       })
